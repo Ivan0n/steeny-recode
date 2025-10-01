4301426ec93f69c2aba1b2d10986fb6103d2cd30
ZastyK <vano19082@gmail.com>
2025-10-01 16:59:09 +0300

    Added threads and new auth 👅

--- __main__.py
+++ __main__.py
@@ -1,10 +1,23 @@
+import threading
+
+
 import musicserver
 import webserver
 
-def start():
-    musicserver.app.run(host='0.0.0.0', port=5000, debug=True)
-    webserver.app.run(host='0.0.0.0', port=2020, debug=True)
+def run_music():
+    musicserver.app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+
+
+def run_web():
+    webserver.app.run(host='0.0.0.0', port=2020, debug=True, use_reloader=False)
 
 
 if __name__ == '__main__':
-    start()
+    servermusic = threading.Thread(target=run_music)
+    serverweb = threading.Thread(target=run_web)
+
+    servermusic.start()
+    serverweb.start()
+
+    servermusic.join()
+    serverweb.join()
